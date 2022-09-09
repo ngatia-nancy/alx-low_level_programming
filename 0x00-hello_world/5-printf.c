0c1de3db5e7fa63c72b4a6f5d2a020d1639f0b9e
ngatia-nancy <nancyngatia1@gmail.com>
2022-09-08 17:12:24 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
- *main-program entry point.
+ * main - Entry point
  *
- *Return: 1 on success.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }	
